b9b738fb13664f8e5413c2d499360537b4f40135
Rafael <rafa.jo.souza@hotmail.com>
2022-04-20 14:25:50 -0300

    Listando clientesatraves da API

--- src/app/clientes.service.ts
+++ src/app/clientes.service.ts
@@ -15,26 +15,9 @@ export class ClientesService {
   salvar( cliente: Cliente) : Observable<Cliente> {
     return this.http.post<Cliente>('http://localhost:8080/api/clientes', cliente);
   }
-/*
+
   getClientes() : Observable<Cliente[]> {
-    return null;
+    return this.http.get<Cliente[]>('http://localhost:8080/api/clientes');
   }
-  */
 
-  getClientes() : Cliente[]{
-    let cliente = new Cliente();
-    cliente.id = 1;
-    cliente.nome = "Rafael José de Souza";
-    cliente.cpf = "12908962080";
-    cliente.email = "rafa.jo.souza@hotmail.com";
-    cliente.dataCadastro = "20/08/2022";
-
-    let cliente1 = new Cliente();
-    cliente1.id = 2;
-    cliente1.nome = "Rafael França de Abreu";
-    cliente1.cpf = "52040597093";
-    cliente1.email = "rafa.franca.abreu@gmail.com";
-    cliente1.dataCadastro = "20/08/2022";
-    return [cliente, cliente1];
-  }
 }
